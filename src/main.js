e678b87528a550ecba13b1bd81f0906aebfa17ca
jamestown502502 <jmbenn02@gmail.com>
2026-04-19 19:51:44 -0400

    fix: restore src/main.js

--- src/main.js
+++ src/main.js
@@ -8,11 +8,6 @@ import RhythmScene from './scenes/RhythmScene.js'
 import RoadEventScene from './scenes/RoadEventScene.js'
 import GameOverScene from './scenes/GameOverScene.js'
 import HUDScene from './scenes/HUDScene.js'
-const config = {
-  type: Phaser.AUTO, width: 960, height: 540, parent: 'game-container', pixelArt: true,
-  backgroundColor: '#0a0a0f',
-  physics: { default: 'arcade', arcade: { gravity: { y: 0 }, debug: false } },
-  scene: [BootScene,PreloaderScene,TitleScene,CityHubScene,DialogueScene,RhythmScene,RoadEventScene,GameOverScene,HUDScene]
-}
-const game = new Phaser.Game(config)
+const config={type:Phaser.AUTO,width:960,height:540,parent:'aame-container', pixelArt:true,backgroundColor:'#0a0a0f',physics:{default:'arcade',arcade:{gravity:{y:0},debug:false}},scene:[BootScene,PreloaderScene,TitleScene,CityHubScene,DialogueScene,RhythmScene,RoadEventScene,GameOverScene,HUDScene]}
+const game=new Phaser.Game(config)
 export default game
